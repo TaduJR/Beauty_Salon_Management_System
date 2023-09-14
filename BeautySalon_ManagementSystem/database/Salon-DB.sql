250fd6e28b38b9c8ca3438d74bbe90bc3e026dea
TaduJR <nattysec26@gmail.com>
2023-09-14 23:47:17 +0300

    Updated SQL

--- BeautySalon_ManagementSystem/database/Salon-DB.sql
+++ BeautySalon_ManagementSystem/database/Salon-DB.sql
@@ -2,69 +2,25 @@ CREATE DATABASE SalonTPS;
 USE SalonTPS;
 
 CREATE TABLE ACCOUNT (
-     id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-     phone VARCHAR(20) NOT NULL UNIQUE,
-     password VARCHAR (20) NOT NULL,
-     name VARCHAR (50) NOT NULL,
-     role VARCHAR (10) NOT NULL
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    phone VARCHAR(20) NOT NULL UNIQUE,
+    password VARCHAR (20) NOT NULL,
+    name VARCHAR (50) NOT NULL,
+    role VARCHAR (10) NOT NULL
 );
 
 CREATE TABLE SERVICE (
-     id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-     name VARCHAR(20) NOT NULL,
-     price FLOAT NOT NULL
-);
-CREATE TABLE BookingPayment(
-  Booking_Payment_ID INT PRIMARY KEY  NOT NULL AUTO_INCREMENT,
-  Cust_Name VARCHAR(20),
-  Cust_Status VARCHAR(20),
-  Cust_Amount VARCHAR(20),
-  Cust_Discount VARCHAR(20),
-  Cust_Total VARCHAR(20)
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name VARCHAR(20) NOT NULL,
+    price FLOAT NOT NULL
 );
 
-CREATE TABLE ReservationPayment(
-  Reservation_Payment_ID INT PRIMARY KEY  NOT NULL AUTO_INCREMENT,
-  Cust_Name VARCHAR(20),
-  Cust_Status VARCHAR(20),
-  Cust_Amount VARCHAR(20),
-  Cust_Discount VARCHAR(20),
-  Cust_Total VARCHAR(20)
-);
+CREATE TABLE Booking (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    service_id INT NOT NULL,
+    account_id INT NOT NULL,
+    date Date,
 
-CREATE TABLE Booking(
-  Booking_No INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  Booking_Payment_ID INT NOT NULL,
-  Cust_Name VARCHAR (20) NOT NULL,
-  Cust_Address VARCHAR (20) NOT NULL,
-  Cust_Phone VARCHAR (11) NOT NULL,
-  Service_ID INT NOT NULL,
-  Services_Name VARCHAR (20) NOT NULL,  
-  Employee_Name VARCHAR (20) NOT NULL,  
-  Acc_ID INT NOT NULL,
-  Booking_Date DATE NOT NULL,
-    
-  FOREIGN KEY(Booking_Payment_ID) REFERENCES BookingPayment(Booking_Payment_ID),
-  FOREIGN KEY(Service_ID) REFERENCES Service(Service_ID),
-  FOREIGN KEY(Acc_ID) REFERENCES Account(Acc_ID) 
-    
-);
-
-
-CREATE TABLE Reservation(
-  Reservation_No INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  Reservation_Payment_ID INT NOT NULL,
-  Cust_Name VARCHAR (20) NOT NULL,
-  Cust_Address VARCHAR (20) NOT NULL,
-  Cust_Phone VARCHAR (11) NOT NULL,
-  Service_ID INT  NOT NULL,
-  Services_Name VARCHAR (20) NOT NULL,
-  Employee_Name VARCHAR (20) NOT NULL,
-  Reserve_Time VARCHAR (20) NOT NULL,
-  Reserve_Date DATE NOT NULL,
-  Acc_ID INT NOT NULL,
-    
-  FOREIGN KEY(Reservation_Payment_ID) REFERENCES ReservationPayment(Reservation_Payment_ID),
-  FOREIGN KEY(Service_ID) REFERENCES Service(Service_ID),
-  FOREIGN KEY(Acc_ID) REFERENCES Account(Acc_ID)   
+    FOREIGN KEY(service_id) REFERENCES SERVICE(id),
+    FOREIGN KEY(account_id) REFERENCES ACCOUNT(id)
 );
